35cc0aba439327b4222724bbdf3f4666527998ca
Shikha Pandey <shikhapandey1998@gmail.com>
2019-12-19 14:12:42 +0530

    Add Package.json

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use('*', (req, res) => {
-	res.sendFile(path.resolve('/INV-MGMT-NEW/index.html', 'index.html'))
-} );
+// app.use('*', (req, res) => {
+// 	res.sendFile(path.resolve(_dirname, 'index.html'))
+// } );
 
 app.listen(port);
 console.log('server started');
